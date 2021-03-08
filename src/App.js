4974a90fb2194a69b1674f566e54ac2458a5a396
Melodiean <melodynsita@gmail.com>
2021-03-08 13:12:27 +0300

    Edit App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AuthProvider } from "./context";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import ResetPassword from "./components/ResetPassword";
+import SignUp from "./components/SignUp";
+import Profile from "./components/Profile";
+import Lookbook from "./components/Lookbook";
+import EditProfile from "./components/EditProfile";
 
+import {Box} from '@material-ui/core';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Welcome to Dictionary!</h1>
+      <AuthProvider>
+        <Box display="flex" justifyContent="center">
+
+        <Header />
+        </Box>
+        <Switch>
+          {/* <Route path="/" exact component={Home} /> */}
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={SignUp} />
+          <Route path="/lookbook" exact component={Lookbook} />
+          <Route path="/edit_profile" exact component={EditProfile} />
+          <Route path="/reset_password" exact component={ResetPassword} />
+        </Switch>
+      </AuthProvider>
+   </div>
   );
 }
 
