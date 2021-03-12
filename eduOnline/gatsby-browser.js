b66360925e780f79a78a7df57a49e74a6172986d
Kasasira Precious <kasasirakp40@gmail.com>
2021-03-12 10:04:55 +0300

    updates app config

--- eduOnline/gatsby-browser.js
+++ eduOnline/gatsby-browser.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { StateProvider } from './StateProvider';
 import reducer, { initialState } from './reducer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import fetch from 'whatwg-fetch' // require('whatwg-fetch') // if it's gatsby v2 - https://gatsby.app/no-mixed-modules
+
+import fetch from 'node-fetch';
+
+export const onClientEntry = () => {
+	// Don't need to do anything here, but if you don't
+	// export something, the import won't work.
+}
 
 export const wrapRootElement = ({ element }) => {
 	return (
--- eduOnline/gatsby-node.js
+++ eduOnline/gatsby-node.js
@@ -5,7 +5,31 @@ require('dotenv').config({
 	path: `.env.${process.env.NODE_ENV}`
 });
 
-module.exports.onCreateWebpackConfig = ({ stage, actions }) => {
+module.exports.onPreBootstrap = () => {
+	require('isomorphic-fetch');
+};
+
+module.exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+	if (stage === 'build-html') {
+		actions.setWebpackConfig({
+			module: {
+				rules: [
+					{
+						test: /firebase/,
+						use: 'null-loader'
+					}
+				]
+			},
+			externals: getConfig().externals.concat(function(context, request, callback) {
+				const regex = /^@?firebase(\/(.+))?/;
+				// exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+				if (regex.test(request)) {
+					return callback(null, `umd ${request}`);
+				}
+				callback();
+			})
+		});
+	}
 	actions.setWebpackConfig({
 		resolve: {
 			modules: [ path.resolve(__dirname, 'src'), 'node_modules' ],
